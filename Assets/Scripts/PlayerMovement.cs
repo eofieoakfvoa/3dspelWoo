a71db2b014773a3b85afeac2f3f77cdaa1fa63af
akeaodkbabkoaeh <114763852+yeaswe@users.noreply.github.com>
2024-01-30 23:04:30 +0100

    dd

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,10 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Callbacks;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
+    protected int movementSpeed = 10;
+    protected float horizontalMovement;
+    protected float verticalMovement;
+    private Vector3 MoveDirection;
+    private int StepUpHeight;
+
+    
+    protected float airHangTime;
+    protected float JumpForce = 10;
+
+
+    public Rigidbody playerRigidBody;
     void Start()
     {
         
@@ -13,6 +28,30 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalMovement = Input.GetAxisRaw("Horizontal");
+        verticalMovement = Input.GetAxisRaw("Vertical");
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            OnJump();
+        }
+    }
+    void FixedUpdate()
+    {
+        OnMovement();
+    }
+
+    public void OnJump()
+    {
+        print("WAA");
+        playerRigidBody.AddForce(Vector2.up * JumpForce, ForceMode.Impulse);
+    }
+    public void OnMovement()
+    {
+        MoveDirection = transform.forward * verticalMovement + transform.right * horizontalMovement;
+        playerRigidBody.AddForce(movementSpeed * Time.deltaTime * MoveDirection.normalized, ForceMode.Impulse);
+    }
+    public void OnFire()
+    {
+
     }
 }
