d8bca478a929d7aea3b7cb0512c23d51295f2dd8
akeaodkbabkoaeh <114763852+yeaswe@users.noreply.github.com>
2024-01-31 12:30:44 +0100

    3

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -14,13 +14,21 @@ public class PlayerMovement : MonoBehaviour
     private Vector3 MoveDirection;
     private int StepUpHeight;
 
-    
+
+    [SerializeField]
+    Transform groundChecker;
+
+    [SerializeField]
+    float groundRadius = 1f;
+
+    [SerializeField]
+    LayerMask groundLayer;    
     protected float airHangTime;
     protected float JumpForce = 10;
 
-
     protected Rigidbody playerRigidBody;
     protected Animator Animator;
+    bool isGrounded;
 
     void Start()
     {
@@ -31,9 +39,24 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+        //bool isGrounded = Physics2D.OverlapBox(groundChecker.position, new Vector2(3, groundRadius), 0, groundLayer);
+        if (Physics.OverlapBox(groundChecker.position, new Vector3(groundRadius, 0.1f , groundRadius), Quaternion.identity, groundLayer).Length > 0)
+        {
+            isGrounded = true;
+            playerRigidBody.drag = 0.5f;
+        }
+        else
+        {
+            isGrounded = false;
+            playerRigidBody.drag = 0f;
+        }
+        print(isGrounded);
         horizontalMovement = Input.GetAxisRaw("Horizontal");
         verticalMovement = Input.GetAxisRaw("Vertical");
-        if (Input.GetKeyDown(KeyCode.Space))
+
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
         {
             OnJump();
         }
@@ -43,19 +66,32 @@ public class PlayerMovement : MonoBehaviour
         OnMovement();
     }
 
-    public void OnJump()
+    private void OnJump()
     {
         //Lägg till Hang Time I luften, Fire medans man åker uppåt stoppar en från att åka högre
         playerRigidBody.AddForce(Vector2.up * JumpForce, ForceMode.Impulse);
     }
-    public void OnMovement()
+    private void OnMovement()
     {
         // Långsamare när man strafear eller går bakåt.
         MoveDirection = transform.forward * verticalMovement + transform.right * horizontalMovement;
         playerRigidBody.AddForce(movementSpeed * Time.deltaTime * MoveDirection.normalized, ForceMode.Impulse);
+
+        Vector3 flatVelocity = new Vector3(playerRigidBody.velocity.x, 0f, playerRigidBody.velocity.z);
+        if (flatVelocity.magnitude > movementSpeed)
+        {
+            Vector3 limitedVelocty = flatVelocity.normalized * movementSpeed;
+            playerRigidBody.velocity = new Vector3(limitedVelocty.x, playerRigidBody.velocity.y, limitedVelocty.z);
+        }
     }
-    public void OnFire()
+    private void OnFire()
     {
 
     }
+            private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireCube(groundChecker.position, new Vector3(groundRadius, 0.1f , groundRadius));
+        }
+    
 }
