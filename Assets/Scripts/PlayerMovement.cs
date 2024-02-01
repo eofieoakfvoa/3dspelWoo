8d4d116bfdad4d3277719ac1355acc10b23e76e8
akeaodkbabkoaeh <Richard.nilsson@elev.ga.ntig.se>
2024-02-01 21:14:29 +0100

    cd
    cd

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Callbacks;
@@ -10,13 +11,13 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     [Header("Movement Variables")]
     [SerializeField] private int MovementMode;
-    [SerializeField] GameObject Camera;
-    protected int movementSpeed = 10;
+    [SerializeField] GameObject mainCamera;
+    protected int movementSpeed = 20;
     protected float horizontalMovement;
     protected float verticalMovement;
     private Vector3 MoveDirection;
     private int stepUpHeight;
-
+    private int cameraMode = 1;
 
     [Header("Jump Variables")]
     [SerializeField] Transform groundChecker;
@@ -26,7 +27,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] LayerMask groundLayer;
 
     bool isGrounded;
-    
+
     [Header("Extras")]
     protected float airHangTime;
     protected float JumpForce = 10;
@@ -48,12 +49,12 @@ public class PlayerMovement : MonoBehaviour
         if (Physics.OverlapBox(groundChecker.position, new Vector3(groundRadius, 0.1f, groundRadius), Quaternion.identity, groundLayer).Length > 0)
         {
             isGrounded = true;
-            playerRigidBody.drag = 0.5f;
+            playerRigidBody.drag = 2f;
         }
         else
         {
             isGrounded = false;
-            playerRigidBody.drag = 0f;
+            playerRigidBody.drag = 1f;
         }
         horizontalMovement = Input.GetAxisRaw("Horizontal");
         verticalMovement = Input.GetAxisRaw("Vertical");
@@ -62,6 +63,14 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
         {
             OnJump();
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            cameraMode = 2;
+        }
+        else if (Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            cameraMode = 1;
         }
 
         //ksk istället för att använda dessa så har jag i en animation controller som har olika states, "Running, Walking, Idle, Jumping, Falling" och mer.
@@ -78,40 +87,24 @@ public class PlayerMovement : MonoBehaviour
         //Lägg till Hang Time I luften, Fire medans man åker uppåt stoppar en från att åka högre, bara när man gör en action i luften dock..
         //Gör så den man faller långsamare när man börja närma sig marken, för en mer gracefull landing
         playerRigidBody.AddForce(Vector2.up * JumpForce, ForceMode.Impulse);
-        
+
     }
     private void OnMovement()
     {
-        // Långsamare när man strafear eller går bakåt.
-        if (MovementMode == 1)
+        playerRigidBody.AddForce(movementSpeed * Time.deltaTime * MoveDirection.normalized, ForceMode.Impulse);
+        Vector3 forwardDirection = transform.forward;
+        Vector3 rightDirection = transform.right;
+
+        if (cameraMode == 1)
         {
-            MoveDirection = transform.forward * verticalMovement + transform.right * horizontalMovement;
-            playerRigidBody.AddForce(movementSpeed * Time.deltaTime * MoveDirection.normalized, ForceMode.Impulse);
-
-            Vector3 flatVelocity = new(playerRigidBody.velocity.x, 0f, playerRigidBody.velocity.z);
-            if (flatVelocity.magnitude > movementSpeed)
-            {
-                Vector3 limitedVelocty = flatVelocity.normalized * movementSpeed;
-                playerRigidBody.velocity = new Vector3(limitedVelocty.x, playerRigidBody.velocity.y, limitedVelocty.z);
-            }
-
-        }
-        //Mode två ska vara att cameran har ingen påvärkan på movementen, förns man klickar på höger klick och då blir W hållet där man kollar
-        if (MovementMode == 2)
-        {            
-            print(Camera.transform.rotation.eulerAngles.y);
-            
-            playerRigidBody.AddForce(movementSpeed * Time.deltaTime * MoveDirection.normalized, ForceMode.Impulse);
-            MoveDirection = Vector3.forward * verticalMovement + Vector3.right * horizontalMovement;
+            forwardDirection = mainCamera.transform.forward;
+            forwardDirection.y = 0;
+            rightDirection = mainCamera.transform.right;
+            rightDirection.y = 0;
             transform.rotation = Quaternion.LookRotation(MoveDirection.normalized);
-
-            Vector3 flatVelocity = new(playerRigidBody.velocity.x, 0f, playerRigidBody.velocity.z);
-            if (flatVelocity.magnitude > movementSpeed)
-            {
-                Vector3 limitedVelocty = flatVelocity.normalized * movementSpeed;
-                playerRigidBody.velocity = new Vector3(limitedVelocty.x, playerRigidBody.velocity.y, limitedVelocty.z);
-            }
         }
+
+        MoveDirection = forwardDirection * verticalMovement + rightDirection * horizontalMovement;
     }
     private void OnFire()
     {
