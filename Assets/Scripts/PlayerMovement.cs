ad8098ae2f0113b8e279d53fdf43700db1399ba5
akeaodkbabkoaeh <Richard.nilsson@elev.ga.ntig.se>
2024-02-01 13:19:18 +0100

    Börja med vapen attachment

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,8 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
+    [Header("Movement Variables")]
+    [SerializeField] private int MovementMode;
     protected int movementSpeed = 10;
     protected float horizontalMovement;
     protected float verticalMovement;
@@ -15,18 +17,23 @@ public class PlayerMovement : MonoBehaviour
     private int stepUpHeight;
 
 
+    [Header("Jump Variables")]
     [SerializeField] Transform groundChecker;
 
     [SerializeField] float groundRadius = 1f;
 
     [SerializeField] LayerMask groundLayer;
 
+    bool isGrounded;
+    
+    [Header("Extras")]
     protected float airHangTime;
     protected float JumpForce = 10;
 
     protected Rigidbody playerRigidBody;
     protected Animator Animator;
-    bool isGrounded;
+
+
 
     void Start()
     {
@@ -37,7 +44,6 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
 
-        //bool isGrounded = Physics2D.OverlapBox(groundChecker.position, new Vector2(3, groundRadius), 0, groundLayer);
         if (Physics.OverlapBox(groundChecker.position, new Vector3(groundRadius, 0.1f, groundRadius), Quaternion.identity, groundLayer).Length > 0)
         {
             isGrounded = true;
@@ -57,6 +63,10 @@ public class PlayerMovement : MonoBehaviour
         {
             OnJump();
         }
+
+        //ksk istället för att använda dessa så har jag i en animation controller som har olika states, "Running, Walking, Idle, Jumping, Falling" och mer.
+        Animator.SetFloat("Speed", playerRigidBody.velocity.magnitude);
+        Animator.SetBool("IsGrounded", isGrounded);
     }
     void FixedUpdate()
     {
@@ -65,21 +75,31 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnJump()
     {
-        //Lägg till Hang Time I luften, Fire medans man åker uppåt stoppar en från att åka högre
+        //Lägg till Hang Time I luften, Fire medans man åker uppåt stoppar en från att åka högre, bara när man gör en action i luften dock..
+        //Gör så den man faller långsamare när man börja närma sig marken, för en mer gracefull landing
         playerRigidBody.AddForce(Vector2.up * JumpForce, ForceMode.Impulse);
         
     }
     private void OnMovement()
     {
         // Långsamare när man strafear eller går bakåt.
-        MoveDirection = transform.forward * verticalMovement + transform.right * horizontalMovement;
-        playerRigidBody.AddForce(movementSpeed * Time.deltaTime * MoveDirection.normalized, ForceMode.Impulse);
-
-        Vector3 flatVelocity = new(playerRigidBody.velocity.x, 0f, playerRigidBody.velocity.z);
-        if (flatVelocity.magnitude > movementSpeed)
+        if (MovementMode == 1)
         {
-            Vector3 limitedVelocty = flatVelocity.normalized * movementSpeed;
-            playerRigidBody.velocity = new Vector3(limitedVelocty.x, playerRigidBody.velocity.y, limitedVelocty.z);
+            MoveDirection = transform.forward * verticalMovement + transform.right * horizontalMovement;
+            playerRigidBody.AddForce(movementSpeed * Time.deltaTime * MoveDirection.normalized, ForceMode.Impulse);
+
+            Vector3 flatVelocity = new(playerRigidBody.velocity.x, 0f, playerRigidBody.velocity.z);
+            if (flatVelocity.magnitude > movementSpeed)
+            {
+                Vector3 limitedVelocty = flatVelocity.normalized * movementSpeed;
+                playerRigidBody.velocity = new Vector3(limitedVelocty.x, playerRigidBody.velocity.y, limitedVelocty.z);
+            }
+
+        }
+        //Mode två ska vara att cameran har ingen påvärkan på movementen, förns man klickar på höger klick och då blir W hållet där man kollar
+        if (MovementMode == 2)
+        {
+            
         }
     }
     private void OnFire()
---
+++ Assets/Scripts/WeaponAttachment.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponAttatchment : MonoBehaviour
+{
+    // Ta in vapnet och person, Försök hitta Handle, och Hand, Beroende på hur många som ska attachas 1 eller 2 ksk fler?, Handles "Up" Bör vara vart fingrarna ska vara och dens down ska vara där palm ska vara (jag hoppas). 
+
+}
