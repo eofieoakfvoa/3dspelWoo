7edf1388133be3dc762b18c743d03bebee059cb5
akeaodkbabkoaeh <Richard.nilsson@elev.ga.ntig.se>
2024-03-16 10:40:50 +0100

    gssva :cat:
    MEOWWOMEWO

--- Assets/Scripts/WeaponScripts/BaseWeaponClass.cs
+++ Assets/Scripts/WeaponScripts/BaseWeaponClass.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class WeaponClass
 {
-    protected float _Damage;
     private PlayerCombat playerCombat = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerCombat>();
+    protected float _Damage;
     //Since the game is using multiple weapons that vary from Melee to Ranged, I have implemented a polymorphism way, where the weapon will implement their own code using the Melee and Ranged Script
     public virtual void LeftClickAction(){}
     public virtual void RightClickAction(){}
-    public void OnEquip()
+    public void OnEquip(WeaponClass Weapon)
     {
-        //playerCombat.Weapon; 
+        playerCombat.Weapon = Weapon;    
     }
 
 }
