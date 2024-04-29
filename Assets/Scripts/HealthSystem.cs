3ce09a74f4093bb3c395bec44e6c04e289e43d6a
akeaodkbabkoaeh <Richard.nilsson@elev.ga.ntig.se>
2024-04-29 11:10:59 +0200

    :cat:

--- Assets/Scripts/HealthSystem.cs
+++ Assets/Scripts/HealthSystem.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Scripting;
 
@@ -20,15 +21,41 @@ public class HealthSystem : MonoBehaviour
     }
     public void TakeHealth(float damageToAdd) //Function to Take Health // Dealing Damage
     {
-        
+        if (damageToAdd > 0)
+        {
+            currentHealth -= damageToAdd;
+            if (currentHealth <= 0)
+            {
+                print("Is Dead"); // Ha någon typ av Death State, I guess jag borde typ ha En player Och Enemycontroller som har egna HandleDeaths() som blir callat här ifrån
+            }
+        }
+        else
+        {
+            print("Took 0 or negative damage");
+        }
     }
     public void GiveHealth(float healthToAdd) //Function to Give health // healing
     {
-
+        if (healthToAdd > 0)
+        {
+            currentHealth += healthToAdd;
+            if (currentHealth > maxHealth) //Overheal
+            {
+                currentHealth = maxHealth;
+            }
+        }
+        else
+        {
+            print("Healing done was either 0 or negative");
+        }
     }
     public void ChangeMaxHealthValue(float ValueChange) //Change the MaxHealth Variable
     {
-        
+        maxHealth += ValueChange; //Negative value to decrease max health, postive to increase max health
+        if (maxHealth <= 0)
+        {
+            maxHealth = 1;
+        } 
     }
 
 }
